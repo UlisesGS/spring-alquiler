e7d589fb3b638425749b221f2c6c793459bc57bf
Ulises Gadea <ulisesgadea28@gmail.com>
2023-05-17 20:08:51 -0300

    agregando solo las carpetas

---
+++ alquileres-back/alquiler/src/main/java/com/alquiler/demo/AlquilerAppApplication.java
@@ -0,0 +1,13 @@
+package com.alquiler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerAppApplication.class, args);
+	}
+
+}
