56000fb83a9e8ad318df7a756ad3686a51355869
damian <adamodamian6@gmail.com>
2023-05-18 21:14:24 -0300

    entidad usuario roles

---
+++ alquileres-back/alquiler/src/main/java/com/alquiler/demo/entity/Usuario.java
@@ -0,0 +1,35 @@
+package com.alquiler.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String apellido;
+    @NotBlank
+    private String dni;
+    @NotBlank
+    private String telefono;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @ManyToMany
+    private List<Rol> roles;
+
+
+
+}
