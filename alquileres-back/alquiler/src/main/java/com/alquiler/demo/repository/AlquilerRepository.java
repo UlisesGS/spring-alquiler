9ac0fbcdb02ab103759e9d984d75e6631076d97f
Ulises Gadea <ulisesgadea28@gmail.com>
2023-06-09 19:38:52 -0300

    agregando fotos del back

--- alquileres-back/alquiler/src/main/java/com/alquiler/demo/repository/AlquilerRepository.java
+++ alquileres-back/alquiler/src/main/java/com/alquiler/demo/repository/AlquilerRepository.java
@@ -13,4 +13,6 @@ public interface AlquilerRepository extends JpaRepository<Alquiler, Long> {
 
     @Query ("select a from Alquiler a where a.peticion.propiedad.propietario.id = ?1 and aceptarPeticion = false")
     public List<Alquiler> aceptarPeticion(Long id);
+
+    
 }
